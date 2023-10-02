ebc61997a78df6254344cbad6879fe77995fb063
MistrrrMedia <65423117+MistrrrMedia@users.noreply.github.com>
2023-10-02 15:59:56 -0500

    Added Designs section
    + design assets in /Assets

--- main.js
+++ main.js
@@ -7,8 +7,16 @@ const projects = document.querySelectorAll( ".project-container" );
 const project_descriptions = document.querySelectorAll( ".project-desc" );
 const up_button = document.getElementById( "up" );
 const down_button = document.getElementById( "down" );
+const slideshow_cards = document.querySelectorAll( ".slideshow-card" );
+
 let index = 0;
 let project_index = 0;
+
+for( let i = 0; i < slideshow_cards.legnth; ++i )
+{
+  console.log( slideshow_cards[ i ].id );
+}
+
 
 scrollUpButton.addEventListener( "click", () => {
   if( index > 0 ) cardPrevious(); // stop at card & section 0
@@ -18,87 +26,49 @@ scrollDownButton.addEventListener( "click", () => {
   if( index < sections.length - 1 ) cardNext(); // stop at last card & section
 } ) ;
 
-// /**
-//  * modifies scroll behavior. scroll up to show the next card & section–down to show the previous. 
-//  * debounced by 200ms to prevent the UI from jumping to further cards on a single scroll
-//  */
-// const scroll = debounce( ( e ) => {
-//   if( e.deltaY > 0 && index < cards.length - 1 ) cardNext(); // go next if up scroll
-//   else if( e.deltaY < 0 && index > 0 ) cardPrevious(); // go backward if down scroll
-//   else return; // do nothing
-//   e.preventDefault;
-// }, 50 ); // delay in ms
+let ssnum = 0;
+slideshow();
+function slideshow()
+{
+  slideshow_cards[ ssnum ].classList.add( "fadeOut" );
+  slideshow_cards[ ssnum ].addEventListener( "animationend", () => {
+    ssnum++;
+    if( ssnum >= slideshow_cards.length ) ssnum = 0;
+    slideshow_cards[ ssnum ].classList.remove( "fadeOut" );
+    slideshow_cards[ ssnum ].classList.add( "fadeIn" );
+  }, { once: true } );
+  setTimeout( slideshow, 2000 );
+}
 
-// /**
-//  * @param {*} func function to debounce
-//  * @param {*} wait time in ms to wait before invoking func
-//  * 
-//  * prevents functions from being called too many times within a single time frame. 
-//  * notably, this is used to modify the scroll behavior to prevent multiple sections from 
-//  * being scrolled through on a single scroll on a trackpad/mouse
-//  */
-// function debounce( func, wait )
-// {
-//   let timeout = 0;
-
-//   return function( ...args )
-//   {
-//     const context = this;
-//     clearTimeout( timeout );
-//     timeout = setTimeout( () => func.apply( context, args ), wait );
-//   }
-// }
-
-// window.addEventListener( "wheel", scroll );
-
-let scrolling = 0;
-
-// window.addEventListener( "wheel", function( event ) 
-// {
-//   if( !scrolling )
-//   {
-//     scrolling = 1;
-//     document.body.classList.add( "no-scroll" );
-//     clearTimeout( window.endScrolling );
-//     window.endScrolling - setTimeout ( function() {
-//       scrolling = 0;
-//       document.body.classList.remove( "no-scroll" );
-//     }, 1000 ); 
-//   }
-
-//   if( event.deltaY > 0 && index < cards.length - 1 ) cardNext(); // go next if up scroll
-//   else if( event.deltaY < 0 && index > 0 ) cardPrevious(); // go backward if down scroll
-//   else return; // do nothing
-//   event.preventDefault;
-
-// }, { passive: false } );
+/**
+ * Overrides scrolling behavior, prevents momentum scrolling from scrolling to the next section prematurely
+ * bool: isProcessing, lastScroll
+ */
 
 let isProcessing = 0;
 let lastScroll = 0;
 
 window.addEventListener( "wheel", ( e ) => {
-  const now = Date.now();
+  const now = Date.now()
 
   if( isProcessing || now - lastScroll < 100 )  
   {
     e.preventDefault();
-    return;
-  }
+    return; // do nothing if scroll events occur < 100 ms between each other
+  } 
 
   lastScroll = now;
-  console.log( "poop! " + lastScroll ); // debug for no reason
 
   if( e.deltaY > 0 && index < cards.length - 1 ) cardNext(); // go next if up scroll
   else if( e.deltaY < 0 && index > 0 ) cardPrevious(); // go backward if down scroll
   else return; // do nothing
 
-  
-  isProcessing = 1;
+  isProcessing = 1; // page is scrolling
   e.preventDefault();
 
-  setTimeout( () => {
+  setTimeout( () => { // prevent scrolling behavior from happening for 500 ms
     isProcessing = 0;
-  }, 500 );
+  }, 1000 );
 }, { passive: false } );
 
 function cardNext()
@@ -167,5 +137,10 @@ projects[ 0 ].addEventListener( "click", () =>
 projects[ 1 ].addEventListener( "click", () => 
 {
   if( project_descriptions[ 2 ].style.display == "none" || project_descriptions[ 2 ].style.display == "" ) doFadeOut( project_index, project_index = 2 );
+} );
+
+projects[ 2 ].addEventListener( "click", () => 
+{
+  if( project_descriptions[ 3 ].style.display == "none" || project_descriptions[ 3 ].style.display == "" ) doFadeOut( project_index, project_index = 3 );
 } );
 
